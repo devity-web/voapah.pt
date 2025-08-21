1a32b8919dd8d3a6a8e51fbbfaa254b1cca86e65
Matheus Pedroni <pedroni.dev@gmail.com>
2025-08-21 14:39:28 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import type React from 'react';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+  title: 'Voapah - Drone',
+  description:
+    '',
+  generator: 'v0.app',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <body className="font-sans antialiased">{children}</body>
+    </html>
+  );
+}
